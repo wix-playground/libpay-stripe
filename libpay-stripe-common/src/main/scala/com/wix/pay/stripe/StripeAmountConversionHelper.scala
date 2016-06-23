069a8ca1905cc6a86e2b8382208a875eec4446d5
Natan Silnitsky <natans@wix.com>
2016-06-23 13:57:06 +0300

    base amount conversion on java.util.Currency

--- libpay-stripe-common/src/main/scala/com/wix/pay/stripe/StripeAmountConversionHelper.scala
+++ libpay-stripe-common/src/main/scala/com/wix/pay/stripe/StripeAmountConversionHelper.scala
@@ -1,27 +1,19 @@
 package com.wix.pay.stripe
 
 import java.math.{BigDecimal => JBigDecimal}
+import java.util.Currency
 
 object StripeAmountConversionHelper {
-  private val defaultDecimalPoints: Int = 2
 
-  private val currencyPoints = Set(
-    "BIF", "CLP", "DJF",
-    "GNF", "JPY", "KMF",
-    "KRW", "MGA", "PYG",
-    "RWF", "VND", "VUV",
-    "XAF", "XOF", "XPF")
-
-  def convert(amount: Double, currency: String): Integer = {
-    JBigDecimal.valueOf(amount).movePointRight(resolveDecimalPoints(currency)).intValueExact()
+  def convert(amount: Double, currencyCode: String): Integer = {
+    JBigDecimal.valueOf(amount).movePointRight(resolveDecimalPoints(currencyCode)).intValueExact()
   }
 
-  private def resolveDecimalPoints(currency: String): Int = {
-    val decimalPoints = findCurrency(currency).map(_ => 0)
-    decimalPoints.getOrElse(defaultDecimalPoints)
-  }
-
-  private def findCurrency(currency: String) = {
-    currencyPoints.find(_.equalsIgnoreCase(currency))
+  private def resolveDecimalPoints(currencyCode: String): Int = {
+    if (currencyCode.equalsIgnoreCase("MGA"))
+      return 0
+    else {
+      Currency.getInstance(currencyCode.toUpperCase).getDefaultFractionDigits
+    }
   }
 }
