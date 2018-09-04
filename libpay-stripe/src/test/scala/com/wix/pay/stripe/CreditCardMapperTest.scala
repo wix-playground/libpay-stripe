288817168d3fb4f4eef31057151caf6a50168d42
Artem Pipich <artemp@wix.com>
2018-09-04 15:33:21 +0300

    [BE][libpay] #WCN-2124: Change metadata sent with Stripe Charge

--- libpay-stripe/src/test/scala/com/wix/pay/stripe/CreditCardMapperTest.scala
+++ libpay-stripe/src/test/scala/com/wix/pay/stripe/CreditCardMapperTest.scala
@@ -1,18 +1,25 @@
 package com.wix.pay.stripe
 
-import com.wix.pay.creditcard.{CreditCard, CreditCardOptionalFields, PublicCreditCardOptionalFields, YearMonth}
-import com.wix.pay.stripe.drivers.StripeMatchers
+import java.util
+
+import com.wix.pay.creditcard.{CreditCard, CreditCardOptionalFields, YearMonth}
+import org.specs2.matcher.Matcher
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
 
-class CreditCardMapperTest extends SpecificationWithJUnit with StripeMatchers {
+class CreditCardMapperTest extends SpecificationWithJUnit {
   "CreditCardMapper" should {
-    "create params with all relevant fields " in new ctx{
-      CreditCardMapper.cardToParams(someCreditCard) must haveFieldParams(expectedFields)
+    "create params with all relevant fields " in new ctx {
+      CreditCardMapper.cardToParams(someCreditCard) must
+        havePair("address_line1", billingAddress.get) and
+        havePair("address_zip", billingPostalCode.get) and
+        havePair("name", holderName.get)
     }
 
     "return params without address and zip code as they are empty Strings " in new ctx {
-      CreditCardMapper.cardToParams(emptyFieldsCreditCard) must not(haveAnyEmptyFields)
+      CreditCardMapper.cardToParams(emptyFieldsCreditCard) must
+        not(haveKey("address_line1")) and
+        not(haveKey("address_zip"))
     }
   }
 
@@ -42,6 +49,11 @@ class CreditCardMapperTest extends SpecificationWithJUnit with StripeMatchers {
         billingAddress = Some(""),
         billingPostalCode = Some(""))))
 
-    val expectedFields = PublicCreditCardOptionalFields(holderId, holderName, billingAddress, billingPostalCode)
+    def havePair(key: String, value: AnyRef): Matcher[util.LinkedHashMap[String, Object]] =
+      be_==(value) ^^ ((_: util.LinkedHashMap[String, Object]).get(key))
+
+    def haveKey(key: String): Matcher[util.LinkedHashMap[String, Object]] =
+      beTrue ^^ ((_: util.LinkedHashMap[String, Object]).containsKey(key))
   }
+
 }
